8c179e728b589da9d2c79a7ee0d26da66ee35dcf
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-03-09 20:42:02 +0100

    color switch

--- projects/getting_started/sketch.js
+++ projects/getting_started/sketch.js
@@ -3,12 +3,18 @@ let c;
 function setup() {
   background(220);
   createCanvas(windowWidth, windowHeight);
-  c = circle(windowWidth / 2, windowHeight / 2, 200);
-  t = new TriangleOnCircle({ x: windowWidth / 2, y: windowHeight / 2 }, 100);
+  // c = circle(windowWidth / 2, windowHeight / 2, 200);
+  t = new TriangleOnCircle(
+    { x: windowWidth / 2, y: windowHeight / 2 },
+    windowWidth / 4
+  );
+  r = 0;
+  g = 0;
+  b = 0;
 }
 
 function draw() {
-  frameRate(5);
+  frameRate(60);
   switch (frameCount % 3) {
     case 0:
       line(t.points[0].x, t.points[0].y, t.points[1].x, t.points[1].y);
@@ -20,7 +26,19 @@ function draw() {
       line(t.points[2].x, t.points[2].y, t.points[0].x, t.points[0].y);
       t = new TriangleOnCircle(
         { x: windowWidth / 2, y: windowHeight / 2 },
-        100
+        windowWidth / 4
       );
+  }
+  if (frameCount % 500 === 0) {
+    if (r === 0) {
+      r = 255;
+      g = 255;
+      b = 255;
+    } else {
+      r = 0;
+      g = 0;
+      b = 0;
+    }
+    stroke(r, g, b);
   }
 }
