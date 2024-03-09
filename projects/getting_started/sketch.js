135cc73af19df9f4f3ac4a47978218be492714fa
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-03-09 18:45:15 +0100

    what have I done - v1

--- projects/getting_started/sketch.js
+++ projects/getting_started/sketch.js
@@ -4,12 +4,23 @@ function setup() {
   background(220);
   createCanvas(windowWidth, windowHeight);
   c = circle(windowWidth / 2, windowHeight / 2, 200);
+  t = new TriangleOnCircle({ x: windowWidth / 2, y: windowHeight / 2 }, 100);
 }
 
 function draw() {
   frameRate(5);
-  t = new TriangleOnCircle({ x: windowWidth / 2, y: windowHeight / 2 }, 100);
-  line(t.points[0].x, t.points[0].y, t.points[1].x, t.points[1].y);
-  line(t.points[1].x, t.points[1].y, t.points[2].x, t.points[2].y);
-  line(t.points[2].x, t.points[2].y, t.points[0].x, t.points[0].y);
+  switch (frameCount % 3) {
+    case 0:
+      line(t.points[0].x, t.points[0].y, t.points[1].x, t.points[1].y);
+      break;
+    case 1:
+      line(t.points[1].x, t.points[1].y, t.points[2].x, t.points[2].y);
+      break;
+    case 2:
+      line(t.points[2].x, t.points[2].y, t.points[0].x, t.points[0].y);
+      t = new TriangleOnCircle(
+        { x: windowWidth / 2, y: windowHeight / 2 },
+        100
+      );
+  }
 }
