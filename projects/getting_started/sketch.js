5bce34571c41832f40ea701dbd2513cdfd066464
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-03-09 20:50:09 +0100

    color swithc + variable rad

--- projects/getting_started/sketch.js
+++ projects/getting_started/sketch.js
@@ -3,11 +3,9 @@ let c;
 function setup() {
   background(220);
   createCanvas(windowWidth, windowHeight);
-  // c = circle(windowWidth / 2, windowHeight / 2, 200);
-  t = new TriangleOnCircle(
-    { x: windowWidth / 2, y: windowHeight / 2 },
-    windowWidth / 4
-  );
+
+  rad = windowWidth / 4;
+  t = new TriangleOnCircle({ x: windowWidth / 2, y: windowHeight / 2 }, rad);
   r = 0;
   g = 0;
   b = 0;
@@ -26,10 +24,11 @@ function draw() {
       line(t.points[2].x, t.points[2].y, t.points[0].x, t.points[0].y);
       t = new TriangleOnCircle(
         { x: windowWidth / 2, y: windowHeight / 2 },
-        windowWidth / 4
+        rad
       );
+    // rad = rad * 1.002;
   }
-  if (frameCount % 500 === 0) {
+  if (frameCount % 750 === 0) {
     if (r === 0) {
       r = 255;
       g = 255;
