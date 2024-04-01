1eada21cf88176a8e3473699ecef44cb918ecff9
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-04-01 19:28:22 +0200

    wip: 3 dimensions

--- projects/bouncing_balls/bouncing_ball.js
+++ projects/bouncing_balls/bouncing_ball.js
@@ -1,14 +1,28 @@
 class BouncingBall {
-  constructor(top_left_corner, bottom_right_corner, radius) {
+  constructor(top_left_corner, bottom_right_corner, y) {
+    // 100 y == furthest away
+    // 0 y == close
+    this.y = y;
+
     this.top_left_corner = top_left_corner;
     this.bottom_right_corner = bottom_right_corner;
     this.center = [
       top_left_corner[0] + (bottom_right_corner[0] - top_left_corner[0]) / 2,
       top_left_corner[1] + (bottom_right_corner[1] - top_left_corner[1]) / 2,
     ];
+
+    this.upperBound = map(y, 0, 100, 0, this.top_left_corner[1]);
+    this.lowerBound = map(y, 0, 100, height, this.bottom_right_corner[1]);
+
+    this.leftBound = map(y, 0, 100, 0, this.top_left_corner[0]);
+    this.rightBound = map(y, 0, 100, width, bottom_right_corner[0]);
+
     this.direction = [random(-5, 5), random(-5, 5)];
-    this.speed = 1.5;
-    this.radius = radius;
+    let l = abs(this.direction[0]) + abs(this.direction[1]);
+    this.direction[0] /= l;
+    this.direction[1] /= l;
+    this.speed = map(y, 0, 100, 30, 5);
+    this.radius = map(y, 0, 100, 60, 10);
   }
 
   move() {
@@ -16,15 +30,15 @@ class BouncingBall {
     this.center[1] += this.speed * this.direction[1];
     // x bounce
     if (
-      this.center[0] + this.radius > this.bottom_right_corner[0] ||
-      this.center[0] - this.radius < this.top_left_corner[0]
+      this.center[0] + this.radius > this.rightBound ||
+      this.center[0] - this.radius < this.leftBound
     ) {
       this.direction[0] *= -1;
     }
     // y bounce
     if (
-      this.center[1] + this.radius > this.bottom_right_corner[1] ||
-      this.center[1] - this.radius < this.top_left_corner[1]
+      this.center[1] + this.radius > this.lowerBound ||
+      this.center[1] - this.radius < this.upperBound
     ) {
       this.direction[1] *= -1;
     }
--- projects/bouncing_balls/sketch.js
+++ projects/bouncing_balls/sketch.js
@@ -1,17 +1,66 @@
 function setup() {
   count = 20;
   createCanvas(windowWidth, windowHeight);
-  lowerRightCorner = [(width * 3) / 4, (height * 3) / 4];
-  upperLeftCorner = [width / 4, height / 4];
+  let centerX = width / 2;
+  let centerY = height / 2;
+
+  let rectWidth = width / 6;
+  let rectHeight = height / 6;
+
+  upperLeftCorner = [centerX - rectWidth / 2, centerY - rectHeight / 2];
+  lowerRightCorner = [centerX + rectWidth / 2, centerY + rectHeight / 2];
   balls = [];
+  y = 100;
   for (i = 0; i < count; i++) {
-    balls[i] = new BouncingBall(upperLeftCorner, lowerRightCorner, 10);
+    balls[i] = new BouncingBall(upperLeftCorner, lowerRightCorner, y);
+    y -= random(0, 10);
   }
 }
 
 function draw() {
   background(220);
   rect(upperLeftCorner[0], upperLeftCorner[1], width / 2, height / 2);
+  quad(
+    0,
+    0,
+    upperLeftCorner[0],
+    upperLeftCorner[1],
+    upperLeftCorner[0],
+    lowerRightCorner[1],
+    0,
+    height
+  );
+  quad(
+    0,
+    0,
+    upperLeftCorner[0],
+    upperLeftCorner[1],
+    lowerRightCorner[0],
+    upperLeftCorner[1],
+    width,
+    0
+  );
+  quad(
+    lowerRightCorner[0],
+    upperLeftCorner[1],
+    width,
+    0,
+    width,
+    height,
+    lowerRightCorner[0],
+    lowerRightCorner[1]
+  );
+  quad(
+    upperLeftCorner[0],
+    lowerRightCorner[1],
+    lowerRightCorner[0],
+    lowerRightCorner[1],
+    width,
+    height,
+    0,
+    height
+  );
+
   for (i = 0; i < count; i++) {
     balls[i].move();
     balls[i].show();
