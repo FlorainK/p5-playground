bafeaec9d6a36d57ddbc2c4c69af8bf2ebd1acb6
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-11-24 13:44:43 +0100

    v 0.1

--- projects/noise/sketch.js
+++ projects/noise/sketch.js
@@ -1,11 +1,16 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rect(
-    windowWidth * 0.1,
-    windowHeight * 0.1,
-    windowWidth * 0.8,
-    windowHeight * 0.8
-  );
 }
 
-function draw() {}
+function draw() {
+  if (frameCount % 2000 === 0) {
+    stroke(0, 0, 0);
+    console.log("black");
+  } else if (frameCount % 1000 === 0) {
+    stroke(255, 255, 255);
+    console.log("white");
+  }
+  for (let i = 0; i < 5000; i++) {
+    point(random(0, windowWidth), random(0, windowHeight));
+  }
+}
