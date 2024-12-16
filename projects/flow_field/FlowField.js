78f9998fe1537f159fd5a06dd434e686a60ca585
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-12-16 23:03:20 +0100

    bugfix

--- projects/flow_field/FlowField.js
+++ projects/flow_field/FlowField.js
@@ -1,12 +1,10 @@
 class FlowField {
   constructor(noise, width, height) {
     this.base_noise = noise;
-    this.width = width;
-    this.height = height;
 
     this.matrix = new Array(width);
-    for (let i = 0; i < width; i++) {
-      this.matrix[i] = new Array(height).fill(null);
+    for (let i = 0; i < height; i++) {
+      this.matrix[i] = new Array(width).fill(null);
     }
   }
 
@@ -15,7 +13,7 @@ class FlowField {
       return this.matrix[y][x];
     } else {
       let v = createVector(10, 10);
-      let noise_factor = this.base_noise(y * 0.1, y * 0.1, offset);
+      let noise_factor = this.base_noise(x * 0.1, y * 0.1, offset);
       v.rotate(noise_factor * TWO_PI);
       this.matrix[y][x] = v;
       return v;
