d48d31fdbda65ae369c0b3d6497707b90aefb3cc
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2025-01-02 18:02:35 +0100

    is there a way to pick the spawn coord with perlin?

--- projects/perlin_wave/sketch.js
+++ projects/perlin_wave/sketch.js
@@ -1,7 +1,6 @@
 let pixel_count = 0;
 let offset = 0;
-let co = 255;
-pixels_per_frame = 10000;
+pixels_per_frame = 30000;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,24 +10,16 @@ function draw() {
   loadPixels();
   noiseDetail(16);
   for (var n = 0; n < pixels_per_frame; n++) {
-    // Use height instead of windowHeight
     var x = floor(random(0, windowWidth));
     var y = floor(random(0, windowHeight));
     var index = (x + y * width) * 4;
 
-    var noise_factor = noise(x * 0.004, y * 0.004, offset) / 3 + 0.2;
+    var noise_factor = noise(x * 0.004, y * 0.004, offset) + 0.1;
 
-    if (noise_factor * random(0, 1) > 0.2) {
-      pixels[index] = 0;
-      pixels[index + 1] = 0;
-      pixels[index + 2] = 0;
-      pixels[index + 3] = co;
+    if (noise_factor * random(0, 1) > 0.5) {
+      pixels[index + 3] = pixels[index + 3] ^ 255;
     }
+    offset += 0.002;
   }
   updatePixels();
-  if (frameCount % 720 === 0) {
-    co = co === 0 ? 255 : 0;
-    offset += 1;
-    console.log("switch");
-  }
 }
